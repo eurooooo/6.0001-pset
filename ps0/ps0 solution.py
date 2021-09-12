64e80b5db770415324c81969b5312789d1591218
eurooooo <88925843+eurooooo@users.noreply.github.com>
2021-09-12 13:57:16 +0800

    Add files via upload

---
+++ ps0/ps0 solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 17:14:44 2021
+
+@author: 52900
+"""
+
+import numpy
+x=int(input('enter number x:' ))
+y=int(input('enter number y:' ))
+print('x=',x)
+print('x**y=',x**y)
+print('log（x）=', numpy.log2(x) )
